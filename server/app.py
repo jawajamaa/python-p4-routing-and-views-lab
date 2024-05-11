fa012c3cffa9f3b0ea2e120a79030f0b66700c3f
Timothy Ryon <tim@timryon.com>
2024-05-11 17:52:14 -0400

    finish and all tests pass

--- server/app.py
+++ server/app.py
@@ -7,20 +7,32 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '<h1>Python Operations with Flask Routing and Views</h1>'
-# replace <param> with variable string?
-@app.route('/print/<parameter>')
+
+@app.route('/print/<string>')
 def print_string(string):
     print(string)
     return string
 
-@app.route('/count/<parameter>')
-def count(integer):
-    for i in range(integer):
-        print(f'{i}\n')
+@app.route('/count/<int:num>')
+def count(num):
+    string = ""
+    for i in range(num):
+        string = string + (str(i)+"\n")
+    return string
 
-@app.route('/math/<num1><operation><num2>')
+@app.route('/math/<num1>/<operation>/<num2>')
 def math(num1, operation, num2):
-    num1, operation, num2
+    if operation == "+":
+        return str(int(num1) + int(num2))
+    elif operation == "-":
+        return str(int(num1) - int(num2))
+    elif operation == "*":
+        return str(int(num1) * int(num2))
+    elif operation == "div":
+        return str(int(num1) / int(num2))
+    elif operation == "%":
+        return str(int(num1) % int(num2))
+    
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
