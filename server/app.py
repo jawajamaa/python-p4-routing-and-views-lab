94e5fbf80604aa125bafd63a2b62cc470c765257
Timothy Ryon <tim@timryon.com>
2024-05-10 21:32:40 -0400

    2 tests pass

--- server/app.py
+++ server/app.py
@@ -4,5 +4,23 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+# replace <param> with variable string?
+@app.route('/print/<parameter>')
+def print_string(string):
+    print(string)
+    return string
+
+@app.route('/count/<parameter>')
+def count(integer):
+    for i in range(integer):
+        print(f'{i}\n')
+
+@app.route('/math/<num1><operation><num2>')
+def math(num1, operation, num2):
+    num1, operation, num2
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
